2217dc85791518315f0a6a14acc72ac9ce414efb
camiloo1210 <130024293+camiloo1210@users.noreply.github.com>
2023-05-22 16:25:12 -0500

    Funcionalidad completa e impresion de funciones

--- matriztrans.c
+++ matriztrans.c
@@ -34,5 +34,24 @@ int main() {
     printf("Ingrese el número de columnas de la matriz: ");
     scanf("%d", &columnas);
 
-   
+    int **matriz = (int**)malloc(filas * sizeof(int*));
+    for (int i = 0; i < filas; i++) {
+        matriz[i] = (int*)malloc(columnas * sizeof(int));
+    }
+
+    srand(time(NULL));
+    for (int i = 0; i < filas; i++) {
+        for (int j = 0; j < columnas; j++) {
+            matriz[i][j] = rand() % 101; // Generar valores aleatorios de 0 a 100
+        }
+    }
+
+    printf("Matriz generada:\n");
+    imprimirMatriz(matriz, filas, columnas);
+
+    int **matrizInversa = generarMatrizInversa(matriz, filas, columnas);
+
+    printf("Matriz inversa:\n");
+    imprimirMatriz(matrizInversa, columnas, filas);
+        return 0;
 }
